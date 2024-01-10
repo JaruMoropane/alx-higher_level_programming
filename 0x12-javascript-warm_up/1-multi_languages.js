b983a620191019f46db517c969fb95b1c4bf9d5f
Jaru Moropane <122880925+JaruMoropane@users.noreply.github.com>
2024-01-10 20:38:41 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-/* Will print 3-lines-using console */
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,7 +0,0 @@
-0-javascript_is_amazing.js
-#!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
